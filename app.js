6c6e4da5cd929d666c908ed3defcdfb9ce4b49ed
ErdoganSenturk <erdsen57@gmail.com>
2022-08-26 00:04:46 +0300

    date

--- app.js
+++ app.js
@@ -1,36 +1,37 @@
-function showTime() {
-  const time = new Date();
-  const hour = time.getHours();
-  const min = time.getMinutes();
-  const sec = time.getSeconds();
-  const day = time.getDate();
-  const month = time.getMonth() + 1;
-  const year = time.getFullYear();
-  am_pm = "AM";
+ function getTime() {
+   let time = new Date();
+   let hour = time.getHours();
+   let min = time.getMinutes();
+   let sec = time.getSeconds();
+   let day = time.getDate();
+   let month = time.getMonth()+1 ;
+   console.log(month);
+   let year = time.getFullYear();
+   AM_PM = "AM";
 
-  if (hour > 12) {
-    hour -= 12;
-    am_pm = "PM";
-  }
+   if (hour > 12) {
+     hour -= 12;
+     AM_PM = "PM";
+   }
 
-  if (hour == 0) {
-    hour = 12;
-    am_pm = "AM";
-  }
+   if (hour == 0) {
+     hour = 12;
+     AM_PM = "AM";
+   }
 
-  hour = hour < 10 ? "0" + hour : hour;
-  min = min < 10 ? "0" + min : min;
-  sec = sec < 10 ? "0" + sec : sec;
+   hour = hour < 10 ? "0" + hour : hour;
+   min = min < 10 ? "0" + min : min;
+   sec = sec < 10 ? "0" + sec : sec;
 
-  let fullTime = hour + ":" + min + ":" + sec + " " + am_pm;
-  document.getElementById("clock").innerText = fullTime;
+   let fullTime = hour + ":" + min + ":" + sec + " " + AM_PM;
+   document.querySelector(".day-time").innerText = fullTime;
 
-  day = day < 10 ? "0" + day : day;
-  month = month < 10 ? "0" + month : month;
+   day = day < 10 ? "0" + day : day;
+   month = month < 10 ? "0" + month : month;
 
-  let fullDay = day + " " + "|" + " " + month + " " + "|" + " " + year;
-  document.getElementById("date-time").innerText = fullDay;
+   let fullDay = day + " " + "/" + " " + month + " " + "/" + " " + year;
+   document.querySelector(".date-time").innerText = fullDay;
 
-  setInterval(showTime, 1000);
-}
-showTime();
+   setInterval(getTime, 1000);
+ }
+ getTime();
